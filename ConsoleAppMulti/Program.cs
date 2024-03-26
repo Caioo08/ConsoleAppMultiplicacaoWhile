c722a3c7f53911a08fcbc709c69627b1bce1572b
Usuario <Usuario@CPD02-M02>
2024-03-26 10:46:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMulti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMulti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double multi = 1;
+            int cont = 1;
+            Console.Write("Informe qual número você quer os múltiplos: ");
+            double vezes = double.Parse(Console.ReadLine());
+            while (multi <= 90) {
+                multi = cont * vezes;
+                Console.WriteLine(multi);
+                cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
